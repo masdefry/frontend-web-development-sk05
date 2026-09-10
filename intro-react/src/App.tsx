98fe0db176b7522e838395ad08fb822eb48f1b4c
M Defryan <muhammaddefryan@gmail.com>
2026-09-10 20:45:41 +0700

    day04: add brand colors

--- intro-react/src/App.tsx
+++ intro-react/src/App.tsx
@@ -4,17 +4,17 @@ import Card from './components/Card';
 function App() {
   return (
     <>
-      <button className='btn btn-soft btn-error'>Error</button>
+      <button className='btn btn-soft btn-error bg-secondary'>Error</button>
       <div className='join grid grid-cols-2'>
         <button className='join-item btn btn-outline'>Previous page</button>
         <button className='join-item btn btn-outline'>Next</button>
       </div>
       <HiMenuAlt3 className='text-3xl text-red-500' />
-      <h1 className='text-blue-500 bg-blue-100 md:text-yellow-700 md:bg-yellow-100'>
+      <h1 className='text-primary bg-blue-100 md:text-yellow-700 md:bg-yellow-100'>
         Typescript
       </h1>
 
-      <div className='grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-6'>
+      <div className='grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-6 bg-secondary'>
         <Card packageName={'PREMIUM'} price={29} />
         <Card packageName={'SILVER'} price={35} />
         <Card packageName={'GOLD'} price={40} />
