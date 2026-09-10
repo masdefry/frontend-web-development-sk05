2b1dda475cb7ad5a54410822e55c4e1a2579bf50
M Defryan <muhammaddefryan@gmail.com>
2026-09-10 20:27:41 +0700

    day04: props component

--- intro-react/src/App.tsx
+++ intro-react/src/App.tsx
@@ -1,4 +1,5 @@
-import { HiMenuAlt3 } from "react-icons/hi";
+import { HiMenuAlt3 } from 'react-icons/hi';
+import Card from './components/Card';
 
 function App() {
   return (
@@ -8,10 +9,16 @@ function App() {
         <button className='join-item btn btn-outline'>Previous page</button>
         <button className='join-item btn btn-outline'>Next</button>
       </div>
-      <HiMenuAlt3 className="text-3xl text-red-500" />
-      <h1 className="text-blue-500 bg-blue-100 md:text-yellow-700 md:bg-yellow-100">
+      <HiMenuAlt3 className='text-3xl text-red-500' />
+      <h1 className='text-blue-500 bg-blue-100 md:text-yellow-700 md:bg-yellow-100'>
         Typescript
       </h1>
+
+      <div className='grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-6'>
+        <Card packageName={'PREMIUM'} price={29} />
+        <Card packageName={'SILVER'} price={35} />
+        <Card packageName={'GOLD'} price={40} />
+      </div>
     </>
   );
 }
