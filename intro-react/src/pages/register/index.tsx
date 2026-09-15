188062b9db58a461e6fd463d0089332760c9e02a
M Defryan <muhammaddefryan@gmail.com>
2026-09-15 19:55:47 +0700

    day05: implement useRef

--- intro-react/src/pages/register/index.tsx
+++ intro-react/src/pages/register/index.tsx
@@ -1,14 +1,36 @@
+import { useRef } from "react";
+
 export default function RegisterPage() {
+    const inputUsername = useRef<HTMLInputElement>(null);
+    const inputEmail = useRef<HTMLInputElement>(null); 
+
+    const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); 
+        console.log('Triggerrr');
+        console.log(inputUsername.current?.value);
+        console.log(inputEmail.current?.value); 
+    }
+
   return (
     <>
       <div className='bg-red-100 flex justify-center'>
-        <form className=' w-92'>
+        <form onSubmit={handleRegister} className=' w-92'>
           <fieldset className='fieldset'>
             <legend className='fieldset-legend'>Username</legend>
             <input
               type='text'
               className='input border border-gray-300 w-full'
               placeholder='Type your username'
+              ref={inputUsername}
+            />
+          </fieldset>
+          <fieldset className='fieldset'>
+            <legend className='fieldset-legend'>Email</legend>
+            <input
+              type='text'
+              className='input border border-gray-300 w-full'
+              placeholder='Type your email'
+              ref={inputEmail}
             />
           </fieldset>
           <button className='btn btn-outline btn-secondary mt-3 w-full'>Secondary</button>
